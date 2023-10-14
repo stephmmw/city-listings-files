ba55363decf0b01ea68a0e5d94faec414273e8da
stephmmw <stephenmmw@yahoo.com>
2023-10-14 13:15:20 +0300

    Add accounts styling

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -5,3 +5,32 @@ $(document).ready(function(){
         $year.innerText = $currentYear;
     }
 });
+
+jQuery(function($){
+    // Password Togglers
+    $("#id_password_toggle").click(function(){
+        showHide('id_password');
+    });
+
+    $("#id_confirm_password_toggle").click(function(){
+        showHide('id_confirm_password');
+    });
+})
+// Show and Hide Passsword
+function showHide(id){
+    let $password = document.getElementById(id);
+    let $passwordToggle = document.getElementById(id + '_toggle');
+    let $passwordToggleIcon = document.getElementById(id + '_toggle_icon');
+
+    if($password.getAttribute("type") == 'password'){
+       $password.setAttribute("type", 'text'); 
+       $passwordToggle.setAttribute("title", 'Hide');
+       $passwordToggleIcon.setAttribute("class", 'bi bi-eye');
+    }
+    else{
+        $password.setAttribute("type", 'password'); 
+        $passwordToggle.setAttribute("title", 'Show');
+        $passwordToggleIcon.setAttribute("class", 'bi bi-eye-slash');
+    }
+    
+}
