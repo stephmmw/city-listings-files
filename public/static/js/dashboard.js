182f21ca43a65ed2beb8a342d1aaa657eba61120
stephmmw <stephenmmw@yahoo.com>
2023-10-21 12:40:03 +0300

    Load profile using js.

--- public/static/js/dashboard.js
+++ public/static/js/dashboard.js
@@ -1,5 +1,5 @@
 $(document).load(function(){
-    if(document.getElementById('id_profile_icon')){
+    if(document.getElementById('id_profile_icon') != null){
         let $profileIcon = document.getElementById('id_profile_icon');
         $profileIcon.setAttribute('src', 'https://stephmmw.github.io/city-listings-files/public/static/profile.jpg');
     }
