1ae3098161974a711de7c4461fd431ac4fad60bb
stephmmw <stephenmmw@yahoo.com>
2023-10-22 10:55:50 +0300

    Update dashboard styles

--- public/static/js/dashboard.js
+++
@@ -1,6 +0,0 @@
-$(document).load(function(){
-    if(document.getElementById('id_profile_icon') != null){
-        let $profileIcon = document.getElementById('id_profile_icon');
-        $profileIcon.setAttribute('src', 'https://stephmmw.github.io/city-listings-files/public/static/profile.jpg');
-    }
-})
